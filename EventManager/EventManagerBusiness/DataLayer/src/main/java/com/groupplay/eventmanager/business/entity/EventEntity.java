d5fcc8575652af602a233ae83978543e2c862ebe
bing.zhang <bing1012001@yahoo.com.hk>
2015-07-04 20:49:30 -0400

    add entity

--- EventManager/EventManagerBusiness/DataLayer/src/main/java/com/groupplay/eventmanager/business/entity/EventEntity.java
+++ EventManager/EventManagerBusiness/DataLayer/src/main/java/com/groupplay/eventmanager/business/entity/EventEntity.java
@@ -8,4 +8,85 @@ package com.groupplay.eventmanager.business.entity;
  * To change this template use File | Settings | File Templates.
  */
 public class EventEntity {
+
+
+    private String id;
+
+    private String groupId;
+
+    private String eventTitle;
+
+    private String eventContent;
+
+    private String eventTime;
+
+    private String locationId;
+
+    private boolean isTemporary;
+
+    private String deadline;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public String getEventContent() {
+        return eventContent;
+    }
+
+    public void setEventContent(String eventContent) {
+        this.eventContent = eventContent;
+    }
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    public boolean isTemporary() {
+        return isTemporary;
+    }
+
+    public void setTemporary(boolean temporary) {
+        isTemporary = temporary;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
 }
